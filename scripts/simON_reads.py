cdcfb60eba3d346358952176fe2baebb6be937b8
Astra Bertelli <133636879+AstraBert@users.noreply.github.com>
2023-12-16 14:56:59 +0100

    Update and rename generate_fastq_data.py to scripts/simON_reads.py

--- scripts/simON_reads.py
+++ scripts/simON_reads.py
@@ -3,24 +3,25 @@ from datetime import datetime
 import sys
 import os
 import matplotlib.pyplot as plt
-from genfastqmodule import *
+from simON_reads_module import *
 
 argparse = ArgumentParser()
 argparse.add_argument("-i", "--infile", help="Path to the input fasta file that contains the reference sequence(s)", required=True)
 argparse.add_argument("-snp", "--single_nucleotide_polymorphism", help="Insert a single nucleotide variant; the syntax of this option should be SAMPLE:POS:REF>ALT,SAMPLE:POS:REF>ALT,...,SAMPLE:POS:REF>ALT (it should be separated by commas without blank spaces) where SAMPLE is the header of the sequence (withouth \">\") in the original fasta file, POS is an integer that indicates the position (0-based) of the polymorphic site, REF is the reference allele, ALT is the alternative allele you want to be put. This will generate a diploid-like distribution of variants", required=False, default="NO_SNP")
+argparse.add_argument("-n", "--nreads", help="Insert the number of reads you want to generate for each provided reference sequence: deafult is 2000", required=False, default=2000, type=int)
 
 args = argparse.parse_args()
 
 inf = args.infile
 snpstring = args.single_nucleotide_polymorphism
-
+nr = args.nreads
 
 if __name__=="__main__":
     start = datetime.now()
-    dic=load_data(inf)
-    avgs=seqs_to_file(dic,snpstring)
+    dic=load_data(inf) ## Load reference sequences from infile
+    avgs=seqs_to_file(dic,snpstring) ## Generate reads and print them
     end=datetime.now()
-    plt.style.use('ggplot')
+    plt.style.use('ggplot') ## Plot average read quality as histogram, show it and save it as avg_quality_distribution.png in current directory
     fig,ax=plt.subplots(figsize=(10,5))
     ax.hist(avgs, bins=40, color='blue', edgecolor='black')
     ax.set_xlabel('Quality')
